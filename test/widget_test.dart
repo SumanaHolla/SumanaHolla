95daef05719ef23cfb3bfb27e42ba7f494b68c55
Sumana.holla <sumanaholla99@gmail.com>
2024-07-07 23:53:38 +0530

    added circular menu items

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:project2/main.dart';
+import 'package:pdc_payment/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
