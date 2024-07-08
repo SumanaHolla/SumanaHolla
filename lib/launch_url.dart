fbab4a7463810f3e976b1f774c71fa81023c53f0
Sumana.holla <sumanaholla99@gmail.com>
2024-07-08 22:20:02 +0530

    updated code

--- lib/launch_url.dart
+++ lib/launch_url.dart
@@ -1,11 +1,6 @@
-import 'dart:math';
-
-import 'package:circular_menu/circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-import 'main.dart';
 
 class LaunchUrl extends StatefulWidget {
   const LaunchUrl({super.key});
@@ -49,281 +44,55 @@ class _LaunchUrlState extends State<LaunchUrl>
 
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   backgroundColor: Colors.white,
-    //   appBar: AppBar(
-    //     title: Container(
-    //         width: double.infinity,
-    //         //color: Colors.red,
-    //         child: CircularMenu(
-    //           items: [
-    //             CircularMenuItem(
-    //               boxShadow: [],
-    //               iconSize: 20,
-    //               color: Colors.red,
-    //               icon: Icons.logout, // Logout icon
-    //               onTap: () {
-    //                 _webViewController.clearCache();
-    //                 Navigator.pushReplacement(
-    //                   context,
-    //                   MaterialPageRoute(
-    //                       builder: (BuildContext context) => const MyApp()),
-    //                 );
-    //               },
-    //             ),
-    //           ],
-    //         )
-    //         // Row(
-    //         //   mainAxisAlignment: MainAxisAlignment.end,
-    //         //   children: [
-    //         //     CircularMenuItem(
-    //         //       boxShadow: [],
-    //         //       iconSize: 20,
-    //         //       color: Colors.red,
-    //         //       icon: Icons.logout, // Logout icon
-    //         //       onTap: () {
-    //         //         _webViewController.clearCache();
-    //         //         Navigator.pushReplacement(
-    //         //           context,
-    //         //           MaterialPageRoute(
-    //         //               builder: (BuildContext context) => const MyApp()),
-    //         //         );
-    //         //       },
-    //         //     ),
-    //         //     CircularMenuItem(
-    //         //       boxShadow: [],
-    //         //       iconSize: 20,
-    //         //       color: Colors.red,
-    //         //       icon: Icons.refresh, // Refresh icon
-    //         //       onTap: () {
-    //         //         Navigator.pushReplacement(
-    //         //           context,
-    //         //           MaterialPageRoute(
-    //         //               builder: (BuildContext context) => const MyApp()),
-    //         //         );
-    //         //       },
-    //         //     ),
-    //         //   ],
-    //         // ),
-    //         ),
-    //   ),
-    //   body: _isSplashScreenLoading
-    //       ? Center(
-    //           child: AnimatedBuilder(
-    //             animation: _animationController,
-    //             builder: (context, child) {
-    //               return Transform.scale(
-    //                 scale: 1.0 + 0.1 * _animationController.value,
-    //                 child: Image.asset(
-    //                   'assets/pdc_payment.jpeg',
-    //                   width: 250,
-    //                   height: 250,
-    //                 ),
-    //               );
-    //             },
-    //           ),
-    //         )
-    //       : SafeArea(
-    //           top: true,
-    //           child: Stack(
-    //             children: [
-    //               InAppWebView(
-    //                 initialUrlRequest: URLRequest(
-    //                   url: Uri.parse('https://pdcgroup.org/login'),
-    //                 ),
-    //                 onWebViewCreated: (InAppWebViewController controller) {
-    //                   _webViewController = controller;
-    //                 },
-    //                 onLoadStart: (controller, url) {
-    //                   setState(() {
-    //                     _isLoading = true;
-    //                   });
-    //                 },
-    //                 onLoadStop: (controller, url) {
-    //                   setState(() {
-    //                     _isLoading = false;
-    //                   });
-    //                 },
-    //               ),
-    //               Visibility(
-    //                   visible: _isLoading,
-    //                   child: const SpinKitCircle(
-    //                     color: Colors.grey,
-    //                     size: 30.0,
-    //                   )),
-    //             ],
-    //           ),
-    //         ),
-    // );
-    // return Scaffold(
-    //   body: SafeArea(
-    //     top: true,
-    //     child: CircularMenu(
-    //       toggleButtonSize: 30,
-    //       startingAngleInRadian: 0.8 * pi,
-    //       endingAngleInRadian: 1 * pi,
-    //       //radius: 2,
-    //       alignment: Alignment.topRight,
-    //       backgroundWidget: _isSplashScreenLoading
-    //           ? Center(
-    //               child: AnimatedBuilder(
-    //                 animation: _animationController,
-    //                 builder: (context, child) {
-    //                   return Transform.scale(
-    //                     scale: 1.0 + 0.1 * _animationController.value,
-    //                     child: Image.asset(
-    //                       'assets/pdc_payment.jpeg',
-    //                       width: 250,
-    //                       height: 250,
-    //                     ),
-    //                   );
-    //                 },
-    //               ),
-    //             )
-    //           : Stack(
-    //               children: [
-    //                 InAppWebView(
-    //                   initialUrlRequest: URLRequest(
-    //                     url: Uri.parse('https://pdcgroup.org/login'),
-    //                   ),
-    //                   onWebViewCreated: (InAppWebViewController controller) {
-    //                     _webViewController = controller;
-    //                   },
-    //                   onLoadStart: (controller, url) {
-    //                     setState(() {
-    //                       _isLoading = true;
-    //                     });
-    //                   },
-    //                   onLoadStop: (controller, url) {
-    //                     setState(() {
-    //                       _isLoading = false;
-    //                     });
-    //                   },
-    //                 ),
-    //                 Visibility(
-    //                     visible: _isLoading,
-    //                     child: const SpinKitCircle(
-    //                       color: Colors.grey,
-    //                       size: 50.0,
-    //                     )),
-    //               ],
-    //             ),
-    //       items: [
-    //         CircularMenuItem(
-    //           boxShadow: [],
-    //           iconSize: 20,
-    //           color: Colors.red,
-    //           icon: Icons.refresh, // Logout icon
-    //           onTap: () {
-    //             Navigator.pushReplacement(
-    //               context,
-    //               MaterialPageRoute(
-    //                   builder: (BuildContext context) => const MyApp()),
-    //             );
-    //           },
-    //         ),
-    //         CircularMenuItem(
-    //           boxShadow: [],
-    //           iconSize: 20,
-    //           color: Colors.red,
-    //           icon: Icons.logout, // Logout icon
-    //           onTap: () {
-    //             _webViewController.clearCache();
-    //             Navigator.pushReplacement(
-    //               context,
-    //               MaterialPageRoute(
-    //                   builder: (BuildContext context) => const MyApp()),
-    //             );
-    //           },
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
-
     return Scaffold(
-      body: SafeArea(
-        top: true,
-        child: _isSplashScreenLoading
-            ? Center(
-                child: AnimatedBuilder(
-                  animation: _animationController,
-                  builder: (context, child) {
-                    return Transform.scale(
-                      scale: 1.0 + 0.1 * _animationController.value,
-                      child: Image.asset(
-                        'assets/pdc_payment.jpeg',
-                        width: 250,
-                        height: 250,
-                      ),
-                    );
+      backgroundColor: Colors.white,
+      body: _isSplashScreenLoading
+          ? Center(
+              child: AnimatedBuilder(
+                animation: _animationController,
+                builder: (context, child) {
+                  return Transform.scale(
+                    scale: 1.0 + 0.1 * _animationController.value,
+                    child: Image.asset(
+                      'assets/pdc_payment.jpeg',
+                      width: 250,
+                      height: 250,
+                    ),
+                  );
+                },
+              ),
+            )
+          : Stack(
+              children: [
+                InAppWebView(
+                  initialUrlRequest: URLRequest(
+                    url: Uri.parse('https://pdcgroup.org/login'),
+                    // url: WebUri.uri(
+                    //   Uri.parse('https://www.carwale.com/new-cars/'),
+                    // ),
+                  ),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    _webViewController = controller;
+                  },
+                  onLoadStart: (controller, url) {
+                    setState(() {
+                      _isLoading = true;
+                    });
+                  },
+                  onLoadStop: (controller, url) {
+                    setState(() {
+                      _isLoading = false;
+                    });
                   },
                 ),
-              )
-            : CircularMenu(
-                toggleButtonSize: 30,
-                startingAngleInRadian: 0.8 * pi,
-                endingAngleInRadian: 1 * pi,
-                alignment: Alignment.topRight,
-                backgroundWidget: Stack(
-                  children: [
-                    InAppWebView(
-                      initialUrlRequest: URLRequest(
-                        url: Uri.parse('https://pdcgroup.org/login'),
-                      ),
-                      onWebViewCreated: (InAppWebViewController controller) {
-                        _webViewController = controller;
-                      },
-                      onLoadStart: (controller, url) {
-                        setState(() {
-                          _isLoading = true;
-                        });
-                      },
-                      onLoadStop: (controller, url) {
-                        setState(() {
-                          _isLoading = false;
-                        });
-                      },
-                    ),
-                    Visibility(
-                        visible: _isLoading,
-                        child: const SpinKitCircle(
-                          color: Colors.grey,
-                          size: 50.0,
-                        )),
-                  ],
-                ),
-                items: [
-                  CircularMenuItem(
-                    boxShadow: [],
-                    iconSize: 20,
-                    color: Colors.red,
-                    icon: Icons.refresh,
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => const MyApp()),
-                      );
-                    },
-                  ),
-                  CircularMenuItem(
-                    boxShadow: [],
-                    iconSize: 20,
-                    color: Colors.red,
-                    icon: Icons.logout,
-                    onTap: () {
-                      _webViewController.clearCache();
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => const MyApp()),
-                      );
-                    },
-                  ),
-                ],
-              ),
-      ),
+                Visibility(
+                    visible: _isLoading,
+                    child: const SpinKitCircle(
+                      color: Colors.grey,
+                      size: 30.0,
+                    )),
+              ],
+            ),
     );
   }
 }
